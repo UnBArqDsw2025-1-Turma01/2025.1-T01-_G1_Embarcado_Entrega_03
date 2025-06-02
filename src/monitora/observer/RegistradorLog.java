24eeda77df76d2ab6111f1a567278e1e9d3d1948
arthurrochamoreira <altinoarthurmoreira@gmail.com>
2025-06-02 02:38:01 -0300

    corrige os textos e containers

--- src/monitora/observer/RegistradorLog.java
+++ src/monitora/observer/RegistradorLog.java
@@ -8,18 +8,21 @@ public class RegistradorLog implements ListenerQueda {
 
     private RegistradorLog() {
         // Construtor privado para implementar o Singleton
-        System.out.println("[RegistradorLog] Instância do RegistradorLog criada.");
+        System.out.println("[RegistradorLog] CONSTRUTOR: Uma nova instância do RegistradorLog foi criada.");
     }
 
     public static RegistradorLog getInstance() {
         if (instance == null) {
             instance = new RegistradorLog();
+        } else {
+            // Mensagem de depuração para quando a instância já existe
+            System.out.println("[RegistradorLog] getInstance(): Retornando a instância existente do RegistradorLog.");
         }
         return instance;
     }
 
     @Override
     public void onQuedaDetectada(Monitoramento m, ResultadoAnalise r) {
-        System.out.println("[RegistradorLog] Queda detectada e registrada no log. " + m.getResumo() + " - Risco: " + r.getRiscoQueda());
+        System.out.println("[RegistradorLog] LOG EVENTO: Queda detectada e registrada. " + m.getResumo() + " - Risco: " + r.getRiscoQueda());
     }
 }
